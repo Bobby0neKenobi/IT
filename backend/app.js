8a93a626d9ddba70540ca4fe00be240825916b71
Bobby0neKenobi <borislav.boydev@gmail.com>
2024-05-26 07:16:01 +0300

    problem fixed

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,5 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/admin', Routes)
-<<<<<<< HEAD
-
-server.listen(3000);
-=======
 app.use(express.static('../frontend'))
 app.listen(3000);
->>>>>>> 9b1558bc2c7ef7d0be82daf6e885d14e2c6ba119
